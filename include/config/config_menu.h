d1d646a483ec3fa17778ff77535f2d02511e0e23
Drahnokks <mathiasduval@hotmail.fr>
2026-05-16 13:23:55 +0200

    move exit course warps

--- include/config/config_menu.h
+++ include/config/config_menu.h
@@ -27,5 +27,5 @@
  * Ensure that the warp exists, or else the game will crash.
  */
 #define EXIT_COURSE_LEVEL LEVEL_CASTLE
-#define EXIT_COURSE_AREA 0x01
-#define EXIT_COURSE_NODE 0x1F
+#define EXIT_COURSE_AREA 0x03
+#define EXIT_COURSE_NODE 0x00
